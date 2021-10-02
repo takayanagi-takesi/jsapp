631aa561e7ee9d04f3e6c0378bdd359426306ffa
takayanagi-takesi <75218115+takayanagi-takesi@users.noreply.github.com>
2021-10-03 08:12:45 +0900

    add

--- sumou/index.js
+++ sumou/index.js
@@ -1,52 +1,85 @@
-const question = `『白鵬』ただしいのはどれ？`
-
-const answers = [
-  'はくほお',
-  'はくほう',
-  'ほほほほう',
-  'はっくほう'
+const quiz = [
+  {
+    question:'『白鵬』ただしいのはどれ？',
+    answers:[ 'はくほお', 'はくほう', 'ほほほほう', 'はっくほう'],
+    correct: 'はくほう'
+  },{
+    question: '『鶴竜』ただしいのはどれ？',
+    answers: ['かくりゆう', 'はくりゅう', 'かくりゅ', 'かくりゅう'],
+    correct: 'かくりゅう'
+  },{
+    question: '『照ノ富士』ただしいのはどれ？',
+    answers: ['てるのふし', 'ですのふじ', 'てるのふじ', 'てるのぶじ'],
+    correct: 'てるのふじ'
+  }
 ];
-const correct = 'はくほう';
+
+// quizがある限り実行、０から開始を指定
+const quizLength = quiz.length;
+let quizIndex = 0;
+
+// １問目の基本文法。（一番上の基礎）
+// const question = `『白鵬』ただしいのはどれ？`
+
+// const answers = [
+//   'はくほお',
+//   'はくほう',
+//   'ほほほほう',
+//   'はっくほう'
+// ];
+// const correct = 'はくほう';
 
 // console.log(document.getElementById('js-question').textContent);
 const $button = document.getElementsByTagName('button');
+let buttonLength = $button.length;
 
 // クイズの問題文、選択肢を定義する
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   // button[0].textContent = answers[0];
   // button[1].textContent = answers[1];
   // button[2].textContent = answers[2];
   // button[3].textContent = answers[3];   ⇓⇓⇓⇓
-  let buttonLength = $button.length;
+  // let buttonLength = $button.length;
+  // ↑buttonLengthにbutton[]を代入
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++; 
 }};
 
 setupQuiz();
 
 const clickHandler = (e) => {
-  if (orrect === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
   window.alert('　○　せいかい！！');
   } else {
   window.alert('ざんねん。。。がんばれ！');
   }
+};
+
+let handlerIndex = 0;
+// const buttonLength = $button.length;
+while (handlerIndex < buttonLength) {
+  $button[0].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
 
 
-$button[0].addEventListener('click', (e) => {
-  clickHandler(e);
-});
-$button[1].addEventListener('click', (e) => {
-  clickHandler(e);
-});
-$button[2].addEventListener('click', (e) => {
-  clickHandler(e);
-});
-$button[3].addEventListener('click', (e) => {
-  clickHandler(e);
-});
+// $button[0].addEventListener('click', (e) => {
+//   clickHandler(e);
+// });
+// $button[1].addEventListener('click', (e) => {
+//   clickHandler(e);
+// });
+// $button[2].addEventListener('click', (e) => {
+//   clickHandler(e);
+// });
+// $button[3].addEventListener('click', (e) => {
+//   clickHandler(e);
+// });
 
 
 // // ぼたんをクリックしたら正誤判定
@@ -79,7 +112,7 @@ $button[3].addEventListener('click', (e) => {
 //     window.alert('ざんねん。。。がんばれ！');
 //   }
 
-  quizIndex++;
+  // quizIndex++;
 
   if (quizIndex < quizLength) {
     setupQuiz();
@@ -87,7 +120,7 @@ $button[3].addEventListener('click', (e) => {
     window.alert('終了！あなたの正解は' + score + '/' + quizLength + 'です！');
   }
 
-};
+
 
 
 let score = 0;
